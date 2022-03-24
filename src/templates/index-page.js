59dfde4157988cbaa24ccf23a39122064fa7cf2b
Jason Prillo <jasonprillo@gmail.com>
2022-03-23 20:01:04 -0400

    cms for index

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,13 +4,14 @@ import Layout from '../components/layout2'
 import { Link} from 'gatsby'
 import background from '../images/hero-background.png'
 import wine from '../images/wine.png'
+import { graphql } from "gatsby"
 
 
 
 
 
-export default function Home() {
-  
+export default function Home({ data }) {
+  const post = data.markdownRemark
   return <div>
        <Helmet>
         <title>Vyntrade  Vineyard to Trade</title>
@@ -21,8 +22,8 @@ export default function Home() {
     <div className="hero" style={{textAlign: "center", backgroundImage: "url(" + background + ")"}}>
    <h1 className="hero-head">VINEYARD TO TRADE</h1>
    <div className="align-center mission" style ={{background: "white"}}>
-      <h2>Our Mission</h2>
-      <p>Create and Implement Profitable Business Opportunities in the US Wine Market for ​Wine Producers through Direct Sales Strategies to Key Retail Clients.</p>
+      <h2>Our Mission</h2>      
+      <p>{post.frontmatter.mission}</p>
     </div>
     </div>
 
@@ -34,7 +35,7 @@ export default function Home() {
       </div>
       <div className="about-description">
         <h3>About Us</h3>
-        <p style={{marginBottom: "2em"}}>VYNTRADE was founded in 2018 to offer consulting to Wine & Spirit Industry Companies.​</p>
+        <p style={{marginBottom: "2em"}}>{post.frontmatter.about}​</p>
        <Link  className= "primary-button" to ="/about">Learn More</Link>
       </div>
       </div>
@@ -48,3 +49,14 @@ export default function Home() {
   
     </div>
 }
+
+export const pageQuery = graphql`
+  query IndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        mission
+        about
+      }
+    }
+  }
+  `
